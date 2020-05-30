da9e26b6d2236079d3b2b319f8050719bd73d694
User <user@Users-MacBook-Pro.local>
2020-05-30 19:39:18 +0100

    Add passenger test added

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
 
@@ -10,6 +13,11 @@ public class FlightTest {
     public void setUp(){
         plane1 = new Plane(PlaneType.BOEING747);
         flight = new Flight(plane1, "FR756", "GLA", "EDI", "10:00");
+    }
+
+    @Test
+    public void canGetPlaneSeats(){
+        assertEquals(20, flight.getPlaneSeats());
     }
 
 }
